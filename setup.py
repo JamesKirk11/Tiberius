8e5845b53ccc63665234d0a52a910676b8e18ed9
James Kirk <jameskirk@live.co.uk>
2023-03-31 15:05:08 -0500

    updated with correct dependencies

--- setup.py
+++ setup.py
@@ -1,2 +1,26 @@
 from setuptools import setup
-setup()
+
+setup(
+    name='Tiberius',
+    version='0.0.1',
+    install_requires=[
+    'argparse',
+    'astropy',
+    'astroscrappy',
+    'batman-package',
+    'corner',
+    'emcee',
+    'exotic-ld',
+    'george',
+    'ipykernel',
+    'ldtk',
+    'jupyter',
+    'matplotlib',
+    'numpy',
+    'pandas',
+    'peakutils',
+    'photutils',
+    'importlib-metadata; python_version >= "3.8"',
+    'scipy'
+    ],
+)
