fixed calculation of errors when using oversampling

src/reduction_utils/spectral_extraction.py
@@ -447,7 +447,7 @@ def extract_trace_flux(frame,trace,aperture_width,background_offset,background_w
         aperture_width_array = aperture_width.copy()
 
     if verbose:
-        plot_frames = [50,nrows/2,nrows-50]
+        plot_frames = [50,nrows//2,nrows-50]
 
         # plot the science frame with apertures overlaid
         plt.figure(figsize=(8,6))
@@ -721,7 +721,7 @@ def extract_trace_flux(frame,trace,aperture_width,background_offset,background_w
                 error.append(np.sqrt(sum(row[aperture_left_hand_edge:aperture_right_hand_edge])/oversampling_factor + (aperture_width/oversampling_factor)*readnoise**2 + dark_current*(aperture_width/oversampling_factor)*exposure_time/3600.)) # raw_flux here takes into account noise from the source and noise from the sky, since this is before background subtraction
 
             elif "JWST" in instrument: # we're using the error frame
-                error.append(np.sqrt(np.sum((error_frame[i][aperture_left_hand_edge:aperture_right_hand_edge])**2)/(oversampling_factor**2)))
+                error.append(np.sqrt(np.sum((error_frame[i][aperture_left_hand_edge:aperture_right_hand_edge]/oversampling_factor)**2)))#/(oversampling_factor**2)))
 
             else: # I'm assuming we're looking at ACAM/EFOSC data and we're including scintillation but not dark current
                 error.append(np.sqrt(sum(row[aperture_left_hand_edge:aperture_right_hand_edge])/oversampling_factor + (aperture_width/oversampling_factor)*readnoise**2 + scintillation**2))