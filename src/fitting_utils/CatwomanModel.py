623f0ae197a3b651e9a31ad2953e5f6574409654
Eva-Maria Ahrer <evamaria.ahrer@gmail.com>
2025-12-01 14:25:51 +0100

    strip down catwoman model to work with the new structure

--- src/fitting_utils/CatwomanModel.py
+++ src/fitting_utils/CatwomanModel.py
@@ -3,8 +3,6 @@
 import numpy as np
 import catwoman
 
-import dynesty
-
 from scipy import optimize,stats
 import matplotlib.pyplot as plt
 
@@ -13,162 +11,56 @@ from fitting_utils import plotting_utils as pu
 from fitting_utils import priors
 
 class CatwomanModel(object):
-    def __init__(self,pars_dict,flux,flux_error,time_array,prior_dict,nested_parameters,k_m_e_equal=False,ld_law="quadratic",cw_fac=None):
+    def __init__(self,param_dict,param_list_free,transit_model_inputs,time_array,cw_fac=None):
 
         """
         
 
         Inputs:
-        pars_dict         - the dictionary of the planet's transit parameters
-        flux              - the light curve flux data points. 
-        flux_error        - the errors on the flux data points to be fitted. 
-        time_array        - the array of time
-        prior_dict        - dictionary with all the information needed to set up the priors (see fitting_input)
-        nested_parameters - parameters needed for dynesty; array of live points per dimension and precision criterion
-        k_m_e_equal       - if morning and evening limb are forced to have the same radius (True or False), default=False
-        ld_law            - the limb darkening law we want to use: linear/quadratic/nonlinear/squareroot, default = quadratic
+        param_dict                   - the dictionary of the planet's transit parameters
+        transit_model_inputs         - the light curve flux data points. 
         cw_fac            - scaling factor for catwoman to make it run faster
         
         
 
         Can return:
         - model calculated on a certain time array
-        - result of catwoman dynesty fitting
         - ... 
         """
 
-        self.pars = pars_dict
-        self.flux_array = flux
-        self.flux_err = flux_error
+        self.param_dict = param_dict
+        self.param_list_free = param_list_free
+        self.transit_model_inputs = transit_model_inputs
         self.time_array = time_array
-        self.prior_dict = prior_dict
-        self.ld_law = ld_law
-        self.k_m_e_equal = k_m_e_equal
-        self.nDims_dict = []
-        self.live_points, self.precision_criterion = nested_parameters
-        self.catwoman_fac = cw_fac
-
-
-        self.namelist = [k for k in self.pars.keys() if self.pars[k] is not None and not isinstance(self.pars[k],float)]
-        self.data = [v for v in self.pars.values() if v is not None and not isinstance(v,float)]
-
-        if isinstance(self.pars['t0'],float):
-            self.fix_t0 = True
-        else:
-            self.fix_t0 = False
-
-        if isinstance(self.pars['u1'],float):
-            self.fix_u1 = True
-        else:
-            self.fix_u1 = False
-
-        if self.ld_law != "linear":
-            if isinstance(self.pars['u2'],float):
-                self.fix_u2 = True
-            else:
-                self.fix_u2 = False
-
-        if self.ld_law == "nonlinear":
-            if isinstance(self.pars['u3'],float):
-                self.fix_u3 = True
-            else:
-                self.fix_u3 = False
-
-            if isinstance(self.pars['u4'],float):
-                self.fix_u4 = True
-            else:
-                self.fix_u4 = False
-                
-        if isinstance(self.pars['infl_err'],float):
-            self.fix_infl_err = True
-        else:
-            self.fix_infl_err = False
-            
+        ld_list = ['u1', 'u2', 'u3', 'u4']
 
         ##### Catwoman initialisation - note this is first outside of model calculation as it is the fastest way
 
         self.catwoman_params = catwoman.TransitParams()
 
-        if self.fix_t0:
-            self.catwoman_params.t0 = self.pars['t0']              #time of inferior conjuction (in days)
-        else:
-            self.catwoman_params.t0 = self.pars['t0'].currVal   #time of inferior conjuction (in days)
-            self.nDims_dict.append('t0')
-            
-        self.catwoman_params.per = self.pars['period']         #orbital period (in days)
-        self.catwoman_params.a = self.pars['aRs']              #semi-major axis (in units of stellar radii)
-        self.catwoman_params.inc = self.pars['inc']            #orbital inclination (in degrees)
-        self.catwoman_params.ecc = self.pars['ecc']            #eccentricity
-        self.catwoman_params.w = self.pars['w']                #longitude of periastron (in degrees)
+        all_params = param_dict.keys()
+        u = []
+        for i in range(len(all_params)):
+            if all_params[i] in param_list_free and all_params[i] not in ld_list:
+                setattr(catwoman_params, all_params[i], self.param_dict[all_params[i]].currVal)
+            if all_params[i] not in param_list_free and all_params[i] not in ld_list:
+                setattr(catwoman_params, all_params[i], self.param_dict[all_params[i]])
         
-        if 'phi' in self.pars:
-            if isinstance(self.pars['phi'],float):
-                self.phi_fix = True
-                self.catwoman_params.phi = self.pars['phi'] 
-            else:
-                self.phi_fix = False
-        else:
-            self.catwoman_params.phi = 90.  #angle of rotation of top semi-circle (in degrees)
-
-        if self.k_m_e_equal:
-            self.catwoman_params.rp = self.pars['k'].currVal   #if morning = evening we only have one radius
-            self.catwoman_params.rp2 = self.pars['k'].currVal
-            self.nDims_dict.append('k')
-        else:
-            self.catwoman_params.rp = self.pars['k_e'].currVal  #top semi-circle radius (in units of stellar radii), if 90 degrees, that's evening
-            self.catwoman_params.rp2 = self.pars['k_m'].currVal #bottom semi-circle radius (in units of stellar radii), if 90 degrees that's morning
-            self.nDims_dict.append('k_e')
-            self.nDims_dict.append('k_m')
-            
-        # limb-darkening 
-        gamma = []
-        if self.fix_u1:
-            gamma.append(self.pars['u1'])
-        else:
-            gamma.append(self.pars['u1'].currVal)
-            self.nDims_dict.append('u1')
-
-        if self.ld_law != "linear":
-            if self.fix_u2:
-                gamma.append(self.pars['u2'])
-            else:
-                gamma.append(self.pars['u2'].currVal)
-                self.nDims_dict.append('u2')
-
-        if self.ld_law == "nonlinear":
-            if self.fix_u3:
-                gamma.append(self.pars['u3'])
-            else:
-                gamma.append(self.pars['u3'].currVal)
-                self.nDims_dict.append('u3')
-
-        if self.ld_law == "nonlinear":
-            if self.fix_u4:
-                gamma.append(self.pars['u4'])
-            else:
-                gamma.append(self.pars['u4'].currVal)
-                self.nDims_dict.append('u4')
-        self.catwoman_params.u = gamma                #limb darkening coefficients [u1, u2, ..,]
-
-        if self.ld_law == "squareroot": # change to match Catwoman's naming
-            self.catwoman_params.limb_dark = "square-root"       #limb darkening model
-        else:
-            self.catwoman_params.limb_dark = self.ld_law
-
-        self.catwoman_model = catwoman.TransitModel(self.catwoman_params, time_array,fac=self.catwoman_fac)    #initializes model
-        
-        if not self.fix_infl_err:
-            self.nDims_dict.append('infl_err') # add for error inflation
-            
-        if not self.fix_phi:
-            self.nDims_dict.append('phi')
-
-        self.nDims = len(self.nDims_dict)
-        
-
+        # for getting the limb-darkening as one array
+        for i in range(len(ld_list)):
+            if ld_list[i] in param_list_free:
+                u.append(self.param_dict[ld_list[i]].currVal)
+            if ld_list[i] not in param_list_free:
+                u.append(self.param_dict[ld_list[i]])
+        self.catwoman_params.limb_dark = transit_model_inputs['ld_law'] 
+        self.catwoman_params.u = u 
     
+        # initalise
+        self.catwoman_model = catwoman.TransitModel(self.catwoman_params, self.time_array,fac=self.catwoman_fac)    #initializes model
+        
 
-    def calc(self,time=None):
+
+    def calc(self,time_array=None, overwrite=False):
 
         """Calculates and returns the evaluated Mandel & Agol transit model, using catwoman.
 
@@ -177,251 +69,11 @@ class CatwomanModel(object):
 
         Returns:
         model - the modelled catwoman light curve"""
-
-        if self.fix_t0:
-            self.catwoman_params.t0 = self.pars['t0']              #time of inferior conjuction (in days)
-        else:
-            self.catwoman_params.t0 = self.pars['t0'].currVal    #time of inferior conjuction (in days)
-
-        if self.k_m_e_equal:
-            self.catwoman_params.rp = self.pars['k'].currVal   #if morning = evening we only have one radius
-            self.catwoman_params.rp2 = self.pars['k'].currVal  
-        else:
-            self.catwoman_params.rp = self.pars['k_e'].currVal  #top semi-circle radius (in units of stellar radii)
-            self.catwoman_params.rp2 = self.pars['k_m'].currVal #bottom semi-circle radius (in units of stellar radii)
-
-        # set up limb darkening coefficients
-        gamma = []
-        if self.fix_u1:
-            gamma.append(self.pars['u1'])
-        else:
-            gamma.append(self.pars['u1'].currVal)
-
-        if self.ld_law != "linear":
-            if self.fix_u2:
-                gamma.append(self.pars['u2'])
-            else:
-                gamma.append(self.pars['u2'].currVal)
-
-        if self.ld_law == "nonlinear":
-            if self.fix_u3:
-                gamma.append(self.pars['u3'])
-            else:
-                gamma.append(self.pars['u3'].currVal)
-
-        if self.ld_law == "nonlinear":
-            if self.fix_u4:
-                gamma.append(self.pars['u4'])
-            else:
-                gamma.append(self.pars['u4'].currVal)
-
-        self.catwoman_params.u = gamma                #limb darkening coefficients [u1, u2]
-
-        if time is not None:
-            if np.any(time != self.time_array): # optionally recalculating catwoman model if the time array has changed
-                self.catwoman_model = catwoman.TransitModel(self.catwoman_params, time, fac=self.catwoman_fac)
-                
-        if not self.phi_fix:
-            self.catwoman_params.phi = self.pars['phi'].currVal  
-
-        model = self.catwoman_model.light_curve(self.catwoman_params)
+        if time_array is not None:
+            new_model = catwoman.TransitModel(self.catwoman_params, time_array,fac=self.catwoman_fac)    # model
+        if time_array is not None and overwrite == True:
+            self.catwoman_model = catwoman.TransitModel(self.catwoman_params, time_array,fac=self.catwoman_fac)    #if we want to continue using that time for the model
+            self.time_array = time_array
+        model = new_model.light_curve(self.catwoman_params)
         return model
 
-    def prior_setup(self, x):
-
-        """Calculates the priors from a uniform cube 
-
-        Inputs:
-        x - the multidimensional cube from the nested sampler 
-
-        Returns:
-        theta - the transformed parameters"""
-        
-        theta = [0] * self.nDims
-        
-        index_run = 0
-        if self.fix_t0 == False:
-            if self.prior_dict['t0_prior'] == 'N':
-                theta[index_run] = priors.GaussianPrior(self.prior_dict['t0_1'],self.prior_dict['t0_2'])(np.array(x[index_run]))
-                index_run += 1
-            elif self.prior_dict['t0_prior'] == 'U':
-                theta[index_run] = priors.UniformPrior(self.prior_dict['t0_1'],self.prior_dict['t0_2'])(np.array(x[index_run]))
-                index_run += 1
-            else:
-                #add exit here 
-                print('Choose either normal or uniform prior') 
-        
-        if self.k_m_e_equal:
-            if self.prior_dict['k_e_prior'] == 'N':
-                theta[index_run] = priors.GaussianPrior(self.prior_dict['k_e_1'],self.prior_dict['k_e_2'])(np.array(x[index_run]))
-                index_run += 1
-            elif self.prior_dict['k_e_prior'] == 'U':
-                theta[index_run] = priors.UniformPrior(self.prior_dict['k_e_1'],self.prior_dict['k_e_2'])(np.array(x[index_run]))
-                index_run += 1
-            else:
-                #add exit here 
-                print('Choose either normal or uniform prior') 
-        else:
-            if self.prior_dict['k_e_prior'] == 'N':
-                theta[index_run] = priors.GaussianPrior(self.prior_dict['k_e_1'],self.prior_dict['k_e_2'])(np.array(x[index_run]))
-                index_run += 1
-            elif self.prior_dict['k_e_prior'] == 'U':
-                theta[index_run] = priors.UniformPrior(self.prior_dict['k_e_1'],self.prior_dict['k_e_2'])(np.array(x[index_run]))
-                index_run += 1
-            else:
-                #add exit here 
-                print('Choose either normal or uniform prior for k_e')
-            
-            if self.prior_dict['k_m_prior'] == 'N':
-                theta[index_run] = priors.GaussianPrior(self.prior_dict['k_m_1'],self.prior_dict['k_m_2'])(np.array(x[index_run]))
-                index_run += 1
-            elif self.prior_dict['k_m_prior'] == 'U':
-                theta[index_run] = priors.UniformPrior(self.prior_dict['k_m_1'],self.prior_dict['k_m_2'])(np.array(x[index_run]))
-                index_run += 1
-            else:
-                #add exit here 
-                print('Choose either normal or uniform prior for k_m')
-            
-
-        if not self.fix_u1:
-            theta[index_run] = priors.GaussianPrior(self.prior_dict['u1'],
-                                                    self.prior_dict['u1_err']*self.prior_dict['ld_unc_multiplier'])(np.array(x[index_run]))
-            index_run += 1
-        if self.ld_law != "linear":
-            if not self.fix_u2:
-                theta[index_run] = priors.GaussianPrior(self.prior_dict['u2'],
-                                                        self.prior_dict['u2_err']*self.prior_dict['ld_unc_multiplier'])(np.array(x[index_run]))
-                index_run += 1
-                
-        if self.ld_law == "nonlinear":
-            if not self.fix_u3:
-                theta[index_run] = priors.GaussianPrior(self.prior_dict['u3'],
-                                                        self.prior_dict['u3_err']*self.prior_dict['ld_unc_multiplier'])(np.array(x[index_run]))
-                index_run += 1
-            if not self.fix_u4:
-                theta[index_run] = priors.GaussianPrior(self.prior_dict['u4'],
-                                                        self.prior_dict['u4_err']+self.prior_dict['ld_unc_multiplier'])(np.array(x[index_run]))
-                index_run += 1
-        if not self.fix_infl_err:
-            if self.prior_dict['error_infl_prior'] == 'N':
-                theta[index_run] = priors.GaussianPrior(self.prior_dict['err_1'],self.prior_dict['err_2'])(np.array(x[index_run]))
-                index_run += 1
-            elif self.prior_dict['error_infl_prior'] == 'U':
-                theta[index_run] = priors.UniformPrior(self.prior_dict['err_1'],self.prior_dict['err_2'])(np.array(x[index_run]))
-                index_run += 1
-            else:
-                #add exit here 
-                print('Choose either normal or uniform prior for err_inflation')
-                
-        if not self.phi_fix:
-            if self.prior_dict['phi_prior'] == 'N':
-                theta[index_run] = priors.GaussianPrior(self.prior_dict['phi_1'],self.prior_dict['phi_2'])(np.array(x[index_run]))
-                index_run += 1
-            elif self.prior_dict['phi_prior'] == 'U':
-                theta[index_run] = priors.UniformPrior(self.prior_dict['phi_1'],self.prior_dict['phi_2'])(np.array(x[index_run]))
-                index_run += 1
-            else:
-                #add exit here 
-                print('Choose either normal or uniform prior for err_inflation')
-
-        return theta
-
-
-    def update_model(self,theta):
-        for i in range(len(theta)):
-            self.pars[self.nDims_dict[i]].currVal = theta[i]
-        return
-
-    def return_curr_parameters(self):
-        return self.pars
-
-    def return_fitted_parameters(self):
-        return self.nDims_dict
-
-
-    def loglikelihood(self,theta):
-        self.update_model(theta)
-        
-        model_values = self.calc(self.time_array)
-        
-        residuals = self.flux_array - model_values
-        
-        if self.fix_infl_err:
-            new_noise = self.flux_err*self.pars['infl_err']
-        else:
-            new_noise = self.flux_err*self.pars['infl_err'].currVal
-        
-        
-        N = len(self.flux_array)
-        logL = -N/2. *  np.log(2*np.pi)
-        logL += - np.nansum(np.log(new_noise)) - np.nansum(residuals**2 / (2 * new_noise**2))
-
-        return logL
-
-
-    def run_dynesty(self):
-        sampler = dynesty.NestedSampler(self.loglikelihood, self.prior_setup, self.nDims,nlive=self.live_points*self.nDims, bootstrap=0)#,sample='rslice')
-        sampler.run_nested(dlogz=self.precision_criterion, print_progress=True) 
-        results = sampler.results
-
-        return results
-        
-    
-
-    def chisq(self,time,flux,flux_err):
-
-        """Evaluate the chi2 of the object.
-
-        Inputs:
-        time - the array of times at which to evaluate the model
-        flux - the flux data points
-        flux_err - the error in the flux data points
-
-        Returns:
-        evaluated chi squared
-        """
-        resids = (flux - self.calc(time))/flux_err
-        return np.sum(resids*resids)
-        
-
-    def reducedChisq(self,time,flux,flux_err):
-        """Evaluate the reduced chi2 of the object.
-
-        Inputs:
-        time - the array of times at which to evaluate the model
-        flux - the flux data points
-        flux_err - the error in the flux data points
-
-        Returns:
-        the evaluated reduced chi squared"""
-        return self.chisq(time,flux,flux_err) / (len(flux) - self.nDims)
-
-
-    def rms(self,time,flux,flux_err=None):
-        """Evaluate the RMS of the residuals
-
-        Inputs:
-        time - the array of times at which to evaluate the model
-        flux - the flux data points
-        flux_err - the error in the flux data points
-
-        Returns:
-        the evaluated RMS of the residuals"""
-
-        resids = (flux - self.calc(time))
-
-        rms = np.sqrt(np.square(resids).mean())
-
-        return rms
-
-class Param(object):
-    '''A Param (parameter) needs a starting value and a current value. However, when first defining the Param object, it takes the starting value as the current value.
-
-    Inputs:
-    startVal: the starting value for the parameter
-
-    Returns:
-    Param object'''
-    def __init__(self,startVal):
-        self.startVal = startVal
-        self.currVal  = startVal 
