35e0cc9affadc5371316791fb08fdedddd6a7588
Eva-Maria Ahrer <evamaria.ahrer@gmail.com>
2025-12-04 13:27:53 +0100

    add update_model function

--- src/fitting_utils/BatmanModel.py
+++ src/fitting_utils/BatmanModel.py
@@ -54,6 +54,34 @@ class BatmanModel(object):
             self.batman_params.u = [u1, u2]
 
         self.batman_model = batman.TransitModel(self.batman_params, self.time_array, nthreads=1)    #initializes model
+    
+    def update_model(self, new_param_dict):
+        self.param_dict = new_param_dict
+        all_params = new_param_dict.keys()
+
+        u = []
+        for i in range(len(all_params)):
+            if all_params[i] in param_list_free and all_params[i] not in ld_list and all_params[i]:
+                setattr(self.batman_params, all_params[i], self.param_dict[all_params[i]].currVal)
+            if all_params[i] not in param_list_free and all_params[i] not in ld_list and all_params[i]:
+                setattr(self.batman_params, all_params[i], self.param_dict[all_params[i]])
+        
+        # for getting the limb-darkening as one array
+        if not self.use_kipping:
+            for i in range(len(ld_list)):
+                if ld_list[i] in param_list_free:
+                    u.append(self.param_dict[ld_list[i]].currVal)
+                if ld_list[i] not in param_list_free and ld_list[i] in all_params:
+                    u.append(self.param_dict[ld_list[i]])
+            self.batman_params.limb_dark = transit_model_inputs['ld_law'] 
+            self.batman_params.u = u 
+        else:
+            u1 = 2*np.sqrt(self.param_dict['u1'].currVal)*self.param_dict['u2'].currVal
+            u2 = np.sqrt(self.param_dict['u1'].currVal)*(1-2*self.param_dict['u2'].currVal)
+            self.batman_params.u = [u1, u2]
+
+        self.batman_model = batman.TransitModel(self.batman_params, self.time_array, nthreads=1)    #initializes model
+        return
 
     def calc(self,time_array=None, overwrite=False):
 
