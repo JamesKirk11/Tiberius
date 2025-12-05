7d83c390ba84f45ea3cbfd13ed695f82449f7894
James Kirk <jameskirk@live.co.uk>
2025-12-05 16:00:07 +0000

    tidied this up a bit and fixed some bugs when running on wb data

--- src/fitting_utils/run_lightcurve_fit.py
+++ src/fitting_utils/run_lightcurve_fit.py
@@ -9,21 +9,27 @@ stopping_bin = int(sys.argv[2])
 
 input_dict = parseInput('fitting_input.txt')
 
+def make_folder(folder_name):
+	if os.path.isdir(folder_name):
+		pass
+	else:
+		os.system("mkdir %s"%folder_name)
+
 if bool(int(input_dict['generate_LDCs'])):
-	os.system("python %s/generate_LDCs.py %d"%(Tiberius_path,i))
+	os.system("python %s/generate_LDCs.py"%(Tiberius_path))
 
 for i in range(starting_bin,stopping_bin):
 	os.system("python %s/light_curve_fit.py %d"%(Tiberius_path,i))
 
 os.system("python %s/plot_output.py -s -st -cp"%Tiberius_path)
 os.system("python %s/model_table_generator.py"%Tiberius_path)
-try:
-	os.system("mkdir tables")
-	os.system("mkdir plots")
-	os.system("mkdir pickled_objects")
-except:
-	pass
-os.system("mv *.pickle pickled_objects/")
-os.system("mv *.txt tables/")
-os.system("mv *.png plots/")
-os.system("mv *.pdf plots/")
+
+make_folder("%s"%input_dict['output_foldername'])
+make_folder("%s/tables"%input_dict['output_foldername'])
+make_folder("%s/plots"%input_dict['output_foldername'])
+make_folder("%s/pickled_objects"%input_dict['output_foldername'])
+
+os.system("mv *.pickle %s/pickled_objects/"%input_dict['output_foldername'])
+os.system("mv *.txt %s/tables/"%input_dict['output_foldername'])
+os.system("mv *.png %s/plots/"%input_dict['output_foldername'])
+os.system("mv *.pdf %s/plots/"%input_dict['output_foldername'])
